11b2877347a43169c21e285b7f4ebe70208ca1aa
Bipul Tamang <bca190619_bipul@achsnepal.edu.np>
2023-05-12 16:26:01 +0545

    completed calculators

--- app/src/main/java/com/example/calculator/feaure/EMI_Calculator.java
+++ app/src/main/java/com/example/calculator/feaure/EMI_Calculator.java
@@ -3,14 +3,51 @@ package com.example.calculator.feaure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.calculator.R;
 
 public class EMI_Calculator extends AppCompatActivity {
+    TextView resulttext;
+    Button solveBtn;
+    EditText principalAmt,Interest,Tenure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emi_calculator);
+        resulttext = findViewById(R.id.EMIresult);
+        solveBtn = findViewById(R.id.calEMIbtn);
+        principalAmt = findViewById(R.id.loanamt);
+        Interest = findViewById(R.id.interestrate);
+        Tenure = findViewById(R.id.monthsTenure);
+
+        principalAmt.setInputType(InputType.TYPE_CLASS_NUMBER);
+        Interest.setInputType(InputType.TYPE_CLASS_NUMBER);
+        Tenure.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        solveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (principalAmt.getText().toString().equals("")||Interest.getText().toString().equals("")||Tenure.getText().toString().equals("")) {
+                    resulttext.setText("Error");
+                }else{
+                    double p = Double.parseDouble(principalAmt.getText().toString());
+                    double r = Double.parseDouble(Interest.getText().toString())/12/100;
+                    double n = Double.parseDouble(Tenure.getText().toString())*12;
+                    double emi;
+
+                    emi = p*r*Math.pow(1+r,n)/(Math.pow(1+r,n)-1);
+                    resulttext.setText(Double.toString(emi));
+                }
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/calculator/feaure/SI_Calculator.java
+++ app/src/main/java/com/example/calculator/feaure/SI_Calculator.java
@@ -3,14 +3,41 @@ package com.example.calculator.feaure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.calculator.R;
 
 public class SI_Calculator extends AppCompatActivity {
-
+    TextView resulttxt;
+    Button SIBtn;
+    EditText principal,time,rate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_si_calculator);
+        resulttxt = findViewById(R.id.SIresult);
+        SIBtn = findViewById(R.id.calSIbtn);
+        principal = findViewById(R.id.princiamt);
+        time = findViewById(R.id.SItime);
+        rate = findViewById(R.id.SIrate);
+
+        SIBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(principal.getText().toString().equals("")||time.getText().toString().equals("")||rate.getText().toString().equals("")){
+                    resulttxt.setText("Error");
+                }else{
+                    double p = Double.parseDouble(principal.getText().toString());
+                    double t = Double.parseDouble(time.getText().toString());
+                    double r = Double.parseDouble(rate.getText().toString());
+                    double si = (p*t*r)/100;
+
+                    resulttxt.setText(Double.toString(si));
+                }
+            }
+        });
     }
 }
